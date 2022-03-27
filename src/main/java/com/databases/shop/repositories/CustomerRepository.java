7dcf96eab555f0110bd011089c0a881b89026c84
Sitkov Illia <illiasitkov@gmail.com>
2022-03-28 01:26:11 +0300

    merge fixes

--- src/main/java/com/databases/shop/repositories/CustomerRepository.java
+++ src/main/java/com/databases/shop/repositories/CustomerRepository.java
@@ -17,20 +17,38 @@ public interface CustomerRepository extends JpaRepository<Customer,Long> {
 //    @Query("SELECT case when count(c)> 0 then true else false end FROM Customer c WHERE c.contacts.email LIKE :email")
 //    boolean existsByEmail(@Param("email") String email);
 
+//my version
+//    @Query(value =
+//            "SELECT COALESCE(MIN(avgCost),0) AS minValue, COALESCE(MAX(avgCost),0) AS maxValue\n" +
+//            "FROM (\n" +
+//            "         SELECT AVG(COALESCE(order_cost,0)) AS avgCost\n" +
+//            "         FROM (\n" +
+//            "                  SELECT customer_id, COALESCE(SUM(prod_quantity * prod_price), 0) AS order_cost\n" +
+//            "                  FROM order_t LEFT OUTER JOIN product_in_order ON order_id = order_t.id\n" +
+//            "                  WHERE status = 'DONE'\n" +
+//            "                  GROUP BY id, customer_id\n" +
+//            "              ) T RIGHT OUTER JOIN customer ON id = T.customer_id\n" +
+//            "         GROUP BY id\n" +
+//            "     ) T1", nativeQuery = true)
+//    MinMaxValues getMinMaxAvgOrderCost();
+
+
 
     @Query(value =
-            "SELECT COALESCE(MIN(avgCost),0) AS minValue, COALESCE(MAX(avgCost),0) AS maxValue\n" +
-            "FROM (\n" +
-            "         SELECT AVG(COALESCE(order_cost,0)) AS avgCost\n" +
-            "         FROM (\n" +
-            "                  SELECT customer_id, COALESCE(SUM(prod_quantity * prod_price), 0) AS order_cost\n" +
-            "                  FROM order_t LEFT OUTER JOIN product_in_order ON order_id = order_t.id\n" +
-            "                  WHERE status = 'DONE'\n" +
-            "                  GROUP BY id, customer_id\n" +
-            "              ) T RIGHT OUTER JOIN customer ON id = T.customer_id\n" +
-            "         GROUP BY id\n" +
-            "     ) T1", nativeQuery = true)
+            "SELECT COALESCE(MIN(COALESCE(avgCost,0)),0) AS minValue, COALESCE(MAX(COALESCE(avgCost,0)),0) AS maxValue\n" +
+                    "FROM customer LEFT OUTER JOIN\n" +
+                    "    (\n" +
+                    "        SELECT AVG(order_cost1) AS avgCost, customer_id\n" +
+                    "        FROM (\n" +
+                    "                 SELECT order_id, SUM(prod_quantity * prod_price) AS order_cost1\n" +
+                    "                 FROM product_in_order\n" +
+                    "                 GROUP BY order_id\n" +
+                    "             ) OrderCost INNER JOIN order_t ON OrderCost.order_id = order_t.id\n" +
+                    "        WHERE status = 'DONE'\n" +
+                    "        GROUP BY customer_id\n" +
+                    "    ) AllCosts ON customer.id = AllCosts.customer_id", nativeQuery = true)
     MinMaxValues getMinMaxAvgOrderCost();
+
 
 
     @Query(value =
